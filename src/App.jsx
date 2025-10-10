132f7c402b78de54764fcb4fce699e182722091f
Abhishek Jaiswar <abhishekjaiswar@gmail.com>
2025-10-10 18:46:48 +0530

    Complete basic routing, add error & pagenotfound page and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
+import PageNotFound from "./components/PageNotFound";
+import AppLayout from "./layout/AppLayout";
+import HomePage from "./pages/HomePage";
+
 const App = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<AppLayout />} errorElement={<ErrorPage />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        {/* Catch-all route for 404s */}
+        <Route path="*" element={<PageNotFound />} />
+      </>
+    )
   );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
